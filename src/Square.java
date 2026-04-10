5951bb4c0d11dc2cc10699d550422b11beb778f0
Anthony Mokhov <131702385+1Am0@users.noreply.github.com>
2026-04-10 16:37:46 +0000

    remove App class and add Board and Square classes

---
+++ src/Square.java
@@ -0,0 +1,14 @@
+public class Square {
+    private boolean isMine;
+    private int value;
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public boolean getIsMine()
+    {
+        return isMine;
+    }
+}
