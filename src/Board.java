816b6c4254c55769a63be00d6aa7ad1f89eaa2fb
Anthony Mokhov <131702385+1Am0@users.noreply.github.com>
2026-04-13 17:05:11 +0000

    Made initial grid and added TODOs for future additions

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,53 @@
 public class Board {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private Square[][] board;
+    private int width;
+    private int height;
+    private int numMines = 25;
+
+    public Board(int size)
+    {
+        width = size;
+        height = size;
+        board = new Square[width][height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                board[i][j] = new Square();
+            }
+        }
+
+        
+    }
+
+    public void Click(int x, int y)
+    {
+        Square hitSquare = board[x][y];
+        if (!hitSquare.getHidden())
+        {
+            // Do nothing, this square has already been unearthed
+            return;
+        }
+        else if (hitSquare.getIsMine())
+        {
+            // TODO: End the game, the player has clicked on a mine
+        }
+        else
+        {
+            // TODO: Unearth this square, it should be safe and continue logic
+        }
+    }
+
+    public void printBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,14 +1,55 @@
 public class Square {
     private boolean isMine;
     private int value;
+    private boolean isHidden;
+
+    public Square()
+    {
+        isMine = false;
+        value = 0;
+        isHidden = true;
+    }
 
     public int getValue()
     {
         return value;
     }
 
+    public void setValue(int _val)
+    {
+        value = _val;
+    }
+
     public boolean getIsMine()
     {
         return isMine;
+    }
+
+    public void setMine()
+    {
+        isMine = true;
+    }
+
+    public boolean getHidden()
+    {
+        return isHidden;
+    }
+
+    public void unhide()
+    {
+        isHidden = false;
+    }
+
+    // For debugging
+    public String toString()
+    {
+        if (isMine)
+        {
+            return "X";
+        }
+        else
+        {
+            return String.valueOf(value);
+        }
     }
 }
